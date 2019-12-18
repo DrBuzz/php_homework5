88009c5ef341d3a3b5e228d96ab21cf07d98a2f5
DrBuzz <omenyzer@gmail.com>
2019-12-18 18:50:13 +0700

    phase 8

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,18 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\TestBasicService;
 
 class TestController extends Controller
 {
     public function  getOne () {
-        $a = new TestOne();
+        $a = new TestBasicService();
         $a->setName('Vladimir');
         $a->setAge(33);
         $a->setSex(true);
         return response()->json($a->getOne());
     }
     public function  setOne (request $request) {
-        $a = new TestOne();
+        $a = new TestBasicService();
         $a->setName($request->get('name'));
         $a->setAge($request->get('age'));
         $a->setSex($request->get('sex'));
@@ -23,34 +24,4 @@ class TestController extends Controller
     }
 }
 
-class TestOne
-{
-    private $name, $age, $sex;
-    public function getOne(){
-        $array = ["name" => $this->name, "age" => $this->age,  "sex" => $this->sex];
-        return $array;
-    }
-    public function getTypes(){
-        $array = ["name" => "string", "age" => "int",  "sex" => "bool"];
-        return $array;
-    }
-    public function getName(){
-        return $this->name;
-    }
-    public function getAge(){
-        return $this->age;
-    }
-    public function getSex(){
-        return $this->sex;
-    }
-    public function setName($name){
-        $this->name=$name;
-    }
-    public function setAge($age){
-        $this->age=$age;
-    }
-    public function setSex($sex){
-        $this->sex=$sex;
-    }
 
-}
