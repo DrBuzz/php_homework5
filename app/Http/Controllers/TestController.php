2a0c7d7e811e0cb4e02a7040ab27981750821b37
DrBuzz <omenyzer@gmail.com>
2019-12-18 17:50:39 +0700

    phases 1 through 4

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function  getOne () {
+        $a = new TestOne();
+        $a->setName('Vladimir');
+        $a->setAge(33);
+        $a->setSex(true);
+        return $a->getOne();
+    }
+}
+
+class TestOne
+{
+    private $name, $age, $sex;
+    public function getOne(){
+        $array = ["name" => $this->name, "age" => $this->age,  "sex" => $this->sex];
+        return $array;
+    }
+    public function getTypes(){
+        $array = ["name" => "string", "age" => "int",  "sex" => "bool"];
+        return $array;
+    }
+    public function getName(){
+        return $this->name;
+    }
+    public function getAge(){
+        return $this->age;
+    }
+    public function getSex(){
+        return $this->sex;
+    }
+    public function setName($name){
+        $this->name=$name;
+    }
+    public function setAge($age){
+        $this->age=$age;
+    }
+    public function setSex($sex){
+        $this->sex=$sex;
+    }
+
+}
